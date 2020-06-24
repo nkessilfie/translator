d5cc0f10a2d5ecfbeb547f03b87abea0e3878ee5
Nathaniel Essel <essilfien18@gmail.com>
2020-06-24 13:32:00 +0000

    initial_2

--- translate_1.py
+++ translate_1.py
@@ -1,2 +1,6 @@
 home = input('----Tap enter to begin translating to a language of your choice----')
-print ("----------------- Let's Begin ---------------------")
+print ("------------------------- Let's Begin ----------------------------- \n"
+       "1. ---------English to French---------\n"
+       "2. ---------English to Spanish--------\n"
+       "3. ---------English to Pidgin---------")
+
