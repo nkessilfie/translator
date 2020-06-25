d5710a951e87b23f2312495ad269e2b57597acae
nkessilfie <58409889+nkessilfie@users.noreply.github.com>
2020-06-25 10:35:59 +0000

    Update test_translate1.py

--- test_translate1.py
+++ test_translate1.py
@@ -7,26 +7,17 @@ class TestTranslate(unittest.TestCase):
     # checking if home is a variable
     def test_homeInput(self):
         self.assertIsNotNone(translate_1.home)
-        self.assertFalse(translate_1.home)
-
+        
     # checking if option is a variable
     def test_optionInput(self):
         self.assertIsNotNone(translate_1.option)
-        self.assertTrue(translate_1.option)
 
     if translate_1.option == 1:
-        # checking the french dictionary
+        # checking the french dictionary keys
         def test_freDict(self):
-            self.assertIn("hello", translate_1.fre_dict)
-            self.assertIn("bye", translate_1.fre_dict)
             self.assertIn("yes", translate_1.fre_dict)
             self.assertNotIn("hi", translate_1.fre_dict)
-            self.assertNotIn("_go_", translate_1.fre_dict)
             self.assertNotIn("123", translate_1.fre_dict)
-
-        # assert insert variable isn't empty
-        def test_frenchWord(self):
-            self.assertIsNotNone(translate_1.french_word)
 
         # assert lower-cases in words
         def test_lowerFreCasing(self):
@@ -45,20 +36,14 @@ class TestTranslate(unittest.TestCase):
         # checking the spanish dictionary
         def test_spaDict(self):
             self.assertIn("hello", translate_1.spa_dict)
-            self.assertIn("bye", translate_1.spa_dict)
-            self.assertIn("yes", translate_1.spa_dict)
             self.assertNotIn("hi", translate_1.spa_dict)
-            self.assertNotIn("_go_", translate_1.spa_dict)
             self.assertNotIn("123", translate_1.spa_dict)
-
-        # assert insert variable isn't empty
-        def test_spanishWord(self):
-            self.assertIsNotNone(translate_1.spanish_word)
 
         # assert lower-cases in words
         def test_lowerSpaCasing(self):
             self.assertEqual(translate_1.spanish_word.islower(), True)
 
+        # assert that a key brings up its value
         def test_checkingFre(self):
             test_dictSpa = translate_1.spa_dict
             for key in test_dictSpa:
@@ -68,16 +53,9 @@ class TestTranslate(unittest.TestCase):
     if translate_1.option == 3:
         # checking the pidgin dictionary
         def test_pidgDict(self):
-            self.assertIn("hello", translate_1.pidg_dict)
             self.assertIn("bye", translate_1.pidg_dict)
-            self.assertIn("yes", translate_1.pidg_dict)
-            self.assertNotIn("hi", translate_1.pidg_dict)
             self.assertNotIn("_go_", translate_1.pidg_dict)
             self.assertNotIn("123", translate_1.pidg_dict)
-
-        # assert insert variable isn't empty
-        def test_pidginWord(self):
-            self.assertIsNotNone(translate_1.pidgin_word)
 
         # assert lower-cases in words
         def test_lowerPidgCasing(self):
